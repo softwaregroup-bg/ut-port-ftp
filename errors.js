5804754a66d71414d3453118c028352f62f76bd2
Elin Angelov <zetxx@belogradchik.biz>
2018-12-06 08:26:03 +0200

    feat: refator (#2)
    * feat: refator
    
    * fix: lint
    
    * fix: linst

--- errors.js
+++ errors.js
@@ -1,13 +1,9 @@
-var create = require('ut-error').define;
-
-var FTP = create('PortFTP');
-var Connection = create('Connection', FTP);
-
-module.exports = {
-    ftp: function(cause) {
-        return new FTP(cause);
-    },
-    connection: function(cause) {
-        return new Connection(cause);
-    }
+'use strict';
+module.exports = ({defineError, fetchErrors}) => {
+    const ftpPort = defineError('ftpPort', undefined, 'Ftp error');
+    defineError('lib.init', ftpPort, 'FTP library has not been initialized');
+    defineError('unknownMethod', ftpPort, 'Unknown method');
+    const connection = defineError('connection', ftpPort, 'Connection');
+    defineError('notReady', connection, 'Connection not ready');
+    return fetchErrors('ftpPort');
 };
