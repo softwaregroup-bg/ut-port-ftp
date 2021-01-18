41aa90c793c5941e610e1dbb3ced5469e74227b6
Kristiyan Todorov <kristiyan.todorov@softwaregroup-bg.com>
2021-01-18 13:19:37 +0200

    refactor: split ftp / sftp clients (#14)

---
+++ ftp.js
@@ -0,0 +1,123 @@
+const FtpClient = require('ftp');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (...params) => class FtpPort extends require('./base')(...params) {
+    async start() {
+        const result = await super.start(...arguments);
+
+        this.client = new FtpClient(this.config.client);
+
+        this.client.on('ready', () => {
+            this.pull(this.exec);
+            this.isReady = true;
+            this.reconnecting = false;
+            this.log.info && this.log.info('Connected');
+        });
+
+        this.client.on('error', e => {
+            this.log.error && this.log.error(e);
+            this.isReady = false;
+            this.reconnecting = false;
+            !this.reconnectInterval && this.reconnect();
+        });
+
+        this.client.on('close', () => {
+            this.log.info && this.log.info('Disconnected');
+            this.isReady = false;
+            this.reconnecting = false;
+            !this.reconnectInterval && this.reconnect();
+        });
+
+        this.client.connect(Object.assign({}, this.config.client, {user: this.config.client.username}));
+
+        return result;
+    }
+
+    stop() {
+        this.client && this.client.destroy();
+        this.reconnectInterval && clearInterval(this.reconnectInterval);
+        return super.stop(...arguments);
+    }
+
+    reconnect() {
+        this.reconnectInterval && clearInterval(this.reconnectInterval); // Ensure no interval will leak
+        this.reconnectInterval = setInterval(() => {
+            if (this.isReady) {
+                clearInterval(this.reconnectInterval);
+                this.reconnectInterval = null;
+            } else if (!this.reconnecting) {
+                this.reconnecting = true;
+                this.log.info && this.log.info('Reconnecting');
+                this.client.connect(Object.assign({}, this.config.client, {user: this.config.client.username}));
+            }
+        }, this.config.reconnectInterval || 10000);
+    }
+
+    get handlers() {
+        const [{utBus, config}] = params;
+        return []
+            .concat(config.namespace)
+            .reduce((handlers, namespace) => ({
+                ...handlers,
+                [`${namespace}.download`](message) {
+                    return new Promise((resolve, reject) => {
+                        this.client.get(message.remoteFile, (err, stream) => {
+                            if (err) return reject(this.errors.ftpPort(err));
+                            if (!message.localFile) {
+                                let buffer = Buffer.alloc(0);
+                                stream.on('data', function(buf) {
+                                    buffer = Buffer.concat([buffer, buf]);
+                                });
+                                stream.once('end', function() {
+                                    resolve(buffer);
+                                });
+                            } else {
+                                const localFilePath = path.join(this.workDir, message.localFile);
+                                stream.once('close', function() {
+                                    return resolve({filepath: localFilePath});
+                                });
+                                stream.once('error', function(e) {
+                                    fs.unlinkSync(localFilePath);
+                                    return reject(e);
+                                });
+                                stream.pipe(fs.createWriteStream(localFilePath));
+                            }
+                        });
+                    });
+                },
+                [`${namespace}.upload`](message) {
+                    return new Promise((resolve, reject) => {
+                        this.client.put(path.join(utBus.config.workDir, message.localFile), message.remoteFile, err => {
+                            if (err) return reject(this.errors.ftpPort(err));
+                            return resolve(true);
+                        });
+                    });
+                },
+                [`${namespace}.append`](message) {
+                    return new Promise((resolve, reject) => {
+                        this.client.append(Buffer.from(message.data, 'utf8'), message.fileName, false, err => {
+                            if (err) return reject(this.errors.ftpPort(err));
+                            return resolve(true);
+                        });
+                    });
+                },
+                [`${namespace}.list`](message) {
+                    return new Promise((resolve, reject) => {
+                        this.client.list(message.remoteDir, (err, list) => {
+                            if (err) return reject(this.errors.ftpPort(err));
+                            return resolve(list);
+                        });
+                    });
+                },
+                [`${namespace}.remove`](message) {
+                    return new Promise((resolve, reject) => {
+                        this.client.delete(message.remoteFile, err => {
+                            if (err) return reject(this.errors.ftpPort(err));
+                            return resolve(true);
+                        });
+                    });
+                }
+            }), {});
+    }
+};
---
+++ test/steps.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const data = fs.readFileSync(path.join(__dirname, 'data', 'test.txt'));
+const {v4: uuid} = require('uuid');
+const filename = `${uuid()}.txt`;
+
+module.exports = async function steps(assert, bus) {
+    fs.writeFileSync(path.join(bus.config.workDir, filename), data);
+    await bus.importMethod('ftp.unknown')({})
+        .catch(e => assert.equals(e.type, 'ftpPort.unknownMethod', 'unknown method'));
+    await bus.importMethod('ftp.list')({remoteDir: 'abcd'})
+        .catch(e => assert.equals(e.type, 'ftpPort', 'missing dir'));
+    await bus.importMethod('ftp.download')({remoteFile: 'nonexistingfile.txt'})
+        .catch(e => assert.equals(e.type, 'ftpPort', 'file does not exist'));
+    await bus.importMethod('ftp.remove')({remoteFile: 'nonexistingfile.txt'})
+        .catch(e => assert.equals(e.type, 'ftpPort', 'file does not exist'));
+
+    await bus.importMethod('ftp.upload')({localFile: filename, remoteFile: filename})
+        .then(r => assert.true(r, 'Successfully upload file'));
+    await bus.importMethod('ftp.append')({data: 'A new line to append \n', fileName: filename})
+        .then(r => assert.true(r, 'Successfully append data to uploaded file'));
+
+    await bus.importMethod('ftp.list')({remoteDir: './'})
+        .then(r => assert.true(r.findIndex(i => (i.name || i.filename) === filename) > -1, 'File is found on remote'));
+    await bus.importMethod('ftp.download')({remoteFile: filename})
+        .then(r => assert.true(Buffer.from(r).length > 0, 'File has content'));
+    await bus.importMethod('ftp.download')({remoteFile: filename, localFile: 'ftpDownload.txt'})
+        .then(({filepath}) => assert.true(fs.readFileSync(filepath).length > 0, 'File was downloaded'));
+
+    await bus.importMethod('ftp.remove')({remoteFile: filename})
+        .then(r => assert.true(r, 'File is successfully removed'));
+    await bus.importMethod('ftp.list')({remoteDir: './'})
+        .then(r => assert.true(r.findIndex(i => (i.name || i.filename) === filename) === -1, 'File is not found on remote'));
+};
