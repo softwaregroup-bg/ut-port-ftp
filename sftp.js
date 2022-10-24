922ca7f40947356f18c21ccc9c52df715e947ac3
ryordanov <rosen.gerov@gmail.com>
2022-10-24 16:28:26 +0300

    Update sftp.js

--- sftp.js
+++ sftp.js
@@ -75,7 +75,7 @@ module.exports = (...params) => class FtpPort extends require('./base')(...param
                     const localFile = params?.localFile || uuid();
                     const localFilePath = path.join(this.workDir, localFile);
                     return new Promise((resolve, reject) =>
-                        sftp.fastGet(params.remoteFile, localFilePath, {chunkSize: 31952}, e => {
+                        sftp.fastGet(params.remoteFile, localFilePath, {...this?.config?.fastGet?.options}, e => {
                             if (e) return reject(this.errors.ftpPort(e));
                             if (!params?.localFile) {
                                 const file = fs.readFileSync(localFilePath);
